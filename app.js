cdc0d965782733a0370837b79603a5476f095671
Harshul Jain <harshulgangwal02@gmail.com>
2022-10-27 13:26:20 +0530

    Added Get and Post request for Students and Teachers

--- app.js
+++ app.js
@@ -12,6 +12,78 @@ const teacherSchema = {
     subject: String
 }
 
+const studentSchema = {
+    name: String,
+    favourite_teacher: []
+}
+
+const Teacher = mongoose.model("Teacher", teacherSchema);
+const Student = mongoose.model("Student", studentSchema);
+
+
+app.route("/students")
+.get(function(req,res){
+    Student.find(function(err, foundStudent){
+        if(!err){
+        res.send(foundStudent);
+        }else{
+            res.send(err);
+        }
+    });
+})
+.post(function(req,res)
+{
+    const newStudent = new Student({
+        name: req.body.name,
+        favourite_teacher: req.body.favourite
+    });
+
+    // if(Teacher.find(function(err, foundTeacher){
+    //     if(!err){
+    //     res.send(foundTeacher);
+    //     }else{
+    //         res.send(err);
+    //     }
+    // });)
+    // newStudent.save(function(err)
+    // {
+    //     if(!err){
+    //         res.send("Successfully added a new Teacher.");
+    //     }else{
+    //         res.send(err);
+    //     }
+    // });
+});
+
+
+app.route("/teachers")
+.get(function(req,res){
+    Teacher.find(function(err, foundTeacher){
+        if(!err){
+        res.send(foundTeacher);
+        }else{
+            res.send(err);
+        }
+    });
+})
+.post(function(req,res)
+{
+    const newTeacher = new Teacher({
+        name: req.body.name,
+        subject: req.body.subject
+    });
+
+    newTeacher.save(function(err)
+    {
+        if(!err){
+            res.send("Successfully added a new Teacher.");
+        }else{
+            res.send(err);
+        }
+    });
+})
+
+
 
 
 app.listen(3000, function() {console.log("Server is running on port 3000")});
